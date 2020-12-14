709e042393a9f32f4021bac5819ec66b1acdb756
hihihibyebye <72005086+hihihibyebye@users.noreply.github.com>
2020-12-14 11:45:22 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var database,dog,happydog,food,dogimg,happydogimg;
+var database,dog,happydog,food,dogimg,happydogimg,foodstoock;
 
 function preload()
 {
@@ -11,13 +11,11 @@ function setup() {
   createCanvas(800, 700);
   database=firebase.database(); 
   dog = createSprite(400,350,50,50);
-  dog = loadImage("img",dogimg);
+  dog.addImage(dogimg);
+  dog.scale=0.3;
 
-  food = database.ref('Food');
-  food.on("value",showError);
-  
-
-  
+  foodstoock = database.ref('food');
+  foodstoock.on("value",readfood,showError);
 }
 
 
@@ -27,19 +25,35 @@ background("green")
   //add styles here
   if(keyDown(UP_ARROW))
   {
-    writefood(-1);
-
+    writefood(food);
+    dog.addImage(happydogimg);
   }
-
+stroke("violet");
+textSize(19);
+text("foodRemaining:"+food,250,500);
 }
 
-function writefood()
-  {
-    database.ref('food').set
-    {
-        'count'
-    }
+function readfood(data)
+{
+  food=data.val();
+}
 
+function writefood(x)
+  {
+   if(x<=0)
+   {
+    x=0;
+   }
+   else
+   {
+    x=x-1;
+   }
+   database.ref('/').update({
+     food:x
+   })
+   
+
+   
   }
 
 
